f318227f70b90920eacba0515c0564d2fc3b47e5
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2015-09-06 16:30:27 +0200

    mp4 to GIF support w/ ffmpeg

--- photosynthgif.sh
+++ photosynthgif.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Photobomb 1.0
+# Photobomb 1.0.1
 # Download Photosynth previews and turn them into a GIF!
 
 if [ $# -lt 1 ]; then
@@ -10,16 +10,33 @@ fi
 command -v convert >/dev/null 2>&1 || { echo >&2 "I require ImageMagick but it's not installed.  Aborting."; exit 1; }
 command -v wget >/dev/null 2>&1 || { echo >&2 "I require wget but it's not installed.  Aborting."; exit 1; }
 
-url_format=$1
 cwd=$(pwd)
 COUNTER=0
-SYNTH=
+SYNTH=$1
 
 cd /tmp/
 mkdir $1
 cd $1
 
-echo "Downloading frames from $1... Standby!"
+if hash ffmpeg 2>/dev/null; then
+        echo "VIDEO: Downloading MP4 Clip..."
+        wget --quiet -k https://cdn4.ps1.photosynth.net/media/$1/packet/thumbs/default/share.mp4 -O $1.mp4
+        if [ -f $1.mp4]; then
+                mkdir frames
+                echo "VIDEO:"Extracting frames from MP4 Clip..."
+                ffmpeg -nostats -loglevel 0 -i $1.mp4 -vf scale=320:-1:flags=lanczos,fps=2 frames/ffout%03d.png
+                echo "VIDEO: Converting frames to GIF..."
+                convert -loop 0 frames/ffout*.png $1_big.gif
+                rm -rf frames $1.mp4
+                mv ./$1_big.gif $cwd
+                echo "VIDEO: Done! Video GIF is served at: $1_big.gif"
+                echo
+        fi
+else
+        echo "FFMpeg not installed, skipping video!"
+fi
+
+echo "PREVIEW: Downloading frames from $1... Standby!"
 
 for i in {1..29}; do
    wget --quiet -k https://cdn3.ps1.photosynth.net/media/$1/packet/thumbs/default/$i.jpg -O frame_$i.jpg || break
@@ -29,9 +46,9 @@ done
 # Zap the last failing image
 rm -rf frame_$[$COUNTER+1].jpg
 
-echo "Converting $COUNTER frames to GIF..."
+echo "PREVIEW: Converting $COUNTER frames to GIF..."
 
-#convert -delay 1 -reverse -layers OptimizePlus -loop 0 frame*.jpg $1.gif
+# convert -delay 1 -reverse -layers OptimizePlus -loop 0 frame*.jpg $1.gif
 convert -delay 1  -layers OptimizePlus -loop 0 frame*.jpg $1.gif
 rm -rf frame*.jpg
 mv ./$1.gif $cwd
@@ -39,4 +56,4 @@ cd ..
 rm -rf $1
 cd $cwd
 
-echo "Done! Photosynth GIF is served at: $1.gif"
+echo "PREVIEW: Done! Photosynth GIF is served at: $1.gif"
